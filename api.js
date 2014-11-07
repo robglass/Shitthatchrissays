a5ab049c61b10d85eabe1c9fb5d5d51df71b08d5
shewlayce <shewlayce@gmail.com>
2014-11-07 15:55:06 -0500

    i r kant spelz

--- api.js
+++ api.js
@@ -63,7 +63,7 @@ var poll = function(){
 	        .on('result', function(data){
 	        	// onlt add to array if the object is not already in it.
 	        	console.log(quotes); 
-	        	if ( quotes.length < date.id){
+	        	if ( quotes.length < data.id){
 	        		quotes.push( data );
 	        	}
 	        })
